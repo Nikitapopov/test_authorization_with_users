11bcce078c7feac9ac84473d8bc82f17aa3ae76f
Nikitapopov <nikitos.popov@inbox.ru>
2020-10-08 13:01:53 +0300

    Updates

---
+++ precache-manifest.fa4720942517feffad7d51435049f100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca12b884f0ff40e39c869b499751eb9",
+    "url": "/test_authorization_with_users/index.html"
+  },
+  {
+    "revision": "afa94d90cce81c3ed0d1",
+    "url": "/test_authorization_with_users/static/css/2.1ce32a85.chunk.css"
+  },
+  {
+    "revision": "2af5844a0c4c49f6e287",
+    "url": "/test_authorization_with_users/static/css/main.b4eeef05.chunk.css"
+  },
+  {
+    "revision": "afa94d90cce81c3ed0d1",
+    "url": "/test_authorization_with_users/static/js/2.745f14c3.chunk.js"
+  },
+  {
+    "revision": "6e30992b0f3803481131f90a7bec4d53",
+    "url": "/test_authorization_with_users/static/js/2.745f14c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af5844a0c4c49f6e287",
+    "url": "/test_authorization_with_users/static/js/main.4b6c9ca8.chunk.js"
+  },
+  {
+    "revision": "7b09a0d8543a702c6ad6",
+    "url": "/test_authorization_with_users/static/js/runtime-main.fdd9cfbd.js"
+  }
+]);
